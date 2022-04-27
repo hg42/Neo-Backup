04a18e82f172943089002ac954103a6363e79480
Harald Gutsche <hg42@gmx.net>
2022-04-27 02:13:00 +0200

    mutate BatchPage to ProgressPage when something is running

--- app/src/main/java/com/machiav3lli/backup/ui/compose/item/ProgressPackageItem.kt
+++ app/src/main/java/com/machiav3lli/backup/ui/compose/item/ProgressPackageItem.kt
@@ -1,6 +1,5 @@
 package com.machiav3lli.backup.ui.compose.item
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
@@ -20,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
@@ -34,34 +31,20 @@ import com.machiav3lli.backup.utils.getFormattedDate
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalCoilApi::class)
 @Composable
-fun BatchPackageItem(
+fun ProgressPackageItem(
     item: Package,
-    restore: Boolean,
-    isApkChecked: Boolean,
-    isDataChecked: Boolean,
-    onClick: (Package, Boolean, Boolean) -> Unit = { _: Package, _: Boolean, _: Boolean -> },
-    onApkClick: (Package, Boolean) -> Unit = { _: Package, _: Boolean -> },
-    onDataClick: (Package, Boolean) -> Unit = { _: Package, _: Boolean -> }
+    onClick: (Package) -> Unit = {}
 ) {
     val packageItem by remember(item) { mutableStateOf(item) }
-    var apkChecked by remember(isApkChecked) { mutableStateOf(isApkChecked) }
-    var dataChecked by remember(isDataChecked) { mutableStateOf(isDataChecked) }
-    val showApk by remember(packageItem) {
+    /*
+    val imageData by remember(packageItem) {
         mutableStateOf(
-            when {
-                packageItem.isSpecial || (restore && !packageItem.hasApk) -> false
-                else -> true
-            }
+            if (packageItem.isSpecial) packageItem.packageInfo.icon
+            else "android.resource://${packageItem.packageName}/${packageItem.packageInfo.icon}"
         )
     }
-    val showData by remember(packageItem) {
-        mutableStateOf(
-            when {
-                restore && !packageItem.hasData -> false
-                else -> true
-            }
-        )
-    }
+    */
+    val state = WorkHandler.packagesState.get(packageItem.packageName) ?: "   "
     packageItem.ensureBackupList()
 
     OutlinedCard(
@@ -70,12 +53,7 @@ fun BatchPackageItem(
         border = BorderStroke(1.dp, MaterialTheme.colorScheme.surface),
         containerColor = MaterialTheme.colorScheme.background,
         elevation = CardDefaults.cardElevation(4.dp),
-        onClick = {
-            val checked = (apkChecked || !showApk) && (dataChecked || !showData)
-            if (showApk) apkChecked = !checked
-            if (showData) dataChecked = !checked
-            onClick(packageItem, apkChecked, dataChecked)
-        }
+        onClick = { onClick(packageItem) },
     ) {
         Row(
             modifier = Modifier
@@ -84,19 +62,12 @@ fun BatchPackageItem(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Checkbox(checked = apkChecked,
-                enabled = showApk,
-                onCheckedChange = {
-                    apkChecked = it
-                    onApkClick(packageItem, it)
-                }
-            )
-            Checkbox(checked = dataChecked,
-                enabled = showData,
-                onCheckedChange = {
-                    dataChecked = it
-                    onDataClick(packageItem, it)
-                }
+            //PackageIcon(item = packageItem, imageData = imageData)
+
+            Text(
+                text = state,
+                fontFamily = FontFamily.Monospace,
+                style = MaterialTheme.typography.titleMedium
             )
 
             Column(
@@ -132,19 +103,18 @@ fun BatchPackageItem(
                         softWrap = true,
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 1,
-                        style = MaterialTheme.typography.bodySmall
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
                     )
-                    AnimatedVisibility(visible = packageItem.hasBackups) {
-                        Text(
-                            text = (packageItem.latestBackup?.backupDate?.getFormattedDate(
-                                false
-                            ) ?: "") + " • ${packageItem.numberOfBackups}",
-                            modifier = Modifier.align(Alignment.CenterVertically),
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            style = MaterialTheme.typography.bodySmall,
-                        )
-                    }
+                    Text(
+                        text = (packageItem.latestBackup?.backupDate?.getFormattedDate(
+                            false
+                        ) ?: "") + " • ${packageItem.numberOfBackups}",
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        style = MaterialTheme.typography.bodySmall,
+                    )
                 }
             }
         }
