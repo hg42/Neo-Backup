6c2f0f0486e0ae4f03bc02c089b1d8b3f832184a
Harald Gutsche <hg42@gmx.net>
2023-01-03 04:11:08 +0100

    use stream in getBackups, don't use Dispatchers.IO to call it

--- app/src/main/java/com/machiav3lli/backup/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/machiav3lli/backup/viewmodels/MainViewModel.kt
@@ -277,7 +277,7 @@ class MainViewModel(
     }
 
     private suspend fun recreateAppInfoList() {
-        withContext(Dispatchers.IO) {
+        withContext(Dispatchers.Default) {
             OABX.beginBusy("recreateAppInfoList")
             val time = measureTimeMillis {
                 appContext.updateAppTables(db.appInfoDao, db.backupDao)
