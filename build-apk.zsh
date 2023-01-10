92381f5552b95d1377ace086f2d90b145535057b
Harald Gutsche <hg42@gmx.net>
2023-01-10 23:49:29 +0100

    LOCAL hg42, apkname, 🐞debug, 🎃test, signing, gitversion, colors, todo

---
+++ build-apk.zsh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+variant=$1
+
+Variant=${(C)variant}
+outdir="./app/build/outputs/apk/$variant"
+savedir="/z/src/android/Neo-Backup--etc/apks/"
+pushdir="/sdcard/Download/NB/"
+
+rm -v $outdir/*
+
+./gradlew --no-daemon --no-build-cache --no-configuration-cache :app:assemble$Variant || exit 1
+
+for apk in $outdir/*.apk(N); do
+  cp -v $apk $savedir
+done
+
+adb devices | while read serial what; do
+  if [[ $what == device ]]; then
+    echo
+    echo "--- $Variant to $serial"
+    echo
+    for apk in $outdir/*.apk(N); do
+      adb -s $serial install        $apk
+      adb -s $serial shell mkdir -p       $pushdir
+      adb -s $serial push           $apk  $pushdir
+    done
+  fi
+done
