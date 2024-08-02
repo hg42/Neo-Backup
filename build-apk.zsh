44569dbca3b8779d2eb78c74711ad0896995362a
hg42 <hg42@users.noreply.github.com>
2024-08-02 17:27:34 +0200

    HG42: change build script to build in fresh git clone outside of work directory

--- build-apk.zsh
+++ build-apk.zsh
@@ -1,15 +1,58 @@
 #!/bin/zsh
 
-variant=$1
+#set -x
+
+variant=${1:-pumpkin}
 
 Variant=${(C)variant}
-outdir="./build/outputs/apk/$variant"
+
+startdir=$(pwd)
+basedir="$(dirname $startdir)/$(basename $startdir)--build-clean/$variant"
+builddir="$basedir/build"
+outdir="$builddir/outputs/apk/$variant"
 savedir="/z/src/android/Neo-Backup--etc/apks/"
 pushdir="/sdcard/Download/NB-apk/"
+marker="$basedir/.marker"
 
-rm -v $outdir/*
+gitstatus=$(git status --porcelain)
+echo $gitstatus
 
-./gradlew --no-daemon --no-build-cache --no-configuration-cache :assemble$Variant || exit 1
+if [[ $(echo $gitstatus | wc -l) != 0 ]]; then
+  print
+  print "git status is not clean"
+  print
+  #exit 1
+  timeout=4
+  print -n "waiting $timeout sec..."
+  repeat $timeout; do
+    print -n "."
+    sleep 1
+  done
+  print "go..."
+  sleep 1
+fi
+
+#set -x
+
+head=$(git rev-parse HEAD)
+
+mkdir -p $basedir
+
+if [[ ! -e $marker || $(cat $marker) != $head ]]; then
+
+  cd $basedir
+  if [[ $(pwd) == $basedir ]]; then
+
+    rm --recursive -v $basedir/*(N) $basedir/.*(N)
+
+    git clone $startdir . &&
+        echo $head >$marker
+
+    ./gradlew --no-daemon --no-build-cache --no-configuration-cache :assemble$Variant ||
+        exit 1
+
+  fi
+fi
 
 for apk in $outdir/*.apk(N); do
   cp -v $apk $savedir
@@ -18,7 +61,7 @@ done
 adb devices | while read serial what; do
   if [[ $what == device ]]; then
     echo
-    echo "--- $Variant to $serial"
+    echo "--- $variant to $serial"
     echo
     for apk in $outdir/*.apk(N); do
       adb -s $serial install        $apk
