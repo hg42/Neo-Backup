c2b9fcdcf585f9f79dd9e55c7ad605f00f19d9ba
hg42 <hg42@users.noreply.github.com>
2024-08-21 23:47:06 +0200

    cleanup

--- src/main/java/com/machiav3lli/backup/OABX.kt
+++ src/main/java/com/machiav3lli/backup/OABX.kt
@@ -310,7 +310,7 @@ class OABX : Application() {
         //TODO hg42 beginBusy(startupMsg)
         hitBusy(60000)
 
-        Plugin.ensureScanned()
+        Plugin.ensureScanned()  // before ShellHandler, because plugins are used there
         initShellHandler()
 
         db = ODatabase.getInstance(applicationContext)
