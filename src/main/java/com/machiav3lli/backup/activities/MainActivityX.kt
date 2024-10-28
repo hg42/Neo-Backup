59b567d1238f0f91acb630486629fe7556b7b574
hg42 <hg42@users.noreply.github.com>
2024-10-28 04:50:46 +0100

    removed pref_allowShadowingDefault (keeping the parameter), retrigger if shadowRoot changes, set devToolsSearch to empty after startup, add backupFolderExists with uri, allow file uri and absolute file path

--- src/main/java/com/machiav3lli/backup/activities/MainActivityX.kt
+++ src/main/java/com/machiav3lli/backup/activities/MainActivityX.kt
@@ -37,6 +37,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.navigation.NavHostController
@@ -69,6 +70,7 @@ import com.machiav3lli.backup.preferences.pref_appTheme
 import com.machiav3lli.backup.tasks.AppActionWork
 import com.machiav3lli.backup.ui.compose.ObservedEffect
 import com.machiav3lli.backup.ui.compose.item.DevTools
+import com.machiav3lli.backup.ui.compose.item.devToolsSearch
 import com.machiav3lli.backup.ui.compose.theme.AppTheme
 import com.machiav3lli.backup.ui.navigation.MainNavHost
 import com.machiav3lli.backup.ui.navigation.NavItem
@@ -253,6 +255,9 @@ class MainActivityX : BaseActivity() {
                                 //TODO hg42 val time = OABX.endBusy(OABX.startupMsg)
                                 //TODO hg42 addInfoLogText("startup: ${"%.3f".format(time / 1E9)} sec")
                             }
+
+                            devToolsSearch.value = TextFieldValue("")   //TODO hg42 hide implementation details
+
                             runOnUiThread { showEncryptionDialog() }
                         }
                     }
@@ -413,7 +418,7 @@ class MainActivityX : BaseActivity() {
     }
 
     fun refreshPackagesAndBackups() {
-        CoroutineScope(Dispatchers.IO).launch {
+        MainScope().launch(Dispatchers.IO) {
             invalidateBackupLocation()
         }
     }
