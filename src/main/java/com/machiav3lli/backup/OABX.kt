d98450681db9d8a2586b8b12dd79b88912942d80
hg42 <hg42@users.noreply.github.com>
2025-06-19 22:05:52 +0200

    reset backup location when not accessible

--- src/main/java/com/machiav3lli/backup/OABX.kt
+++ src/main/java/com/machiav3lli/backup/OABX.kt
@@ -62,6 +62,7 @@ import com.machiav3lli.backup.items.StorageFile
 import com.machiav3lli.backup.plugins.Plugin
 import com.machiav3lli.backup.preferences.pref_busyHitTime
 import com.machiav3lli.backup.preferences.pref_newAndUpdatedNotification
+import com.machiav3lli.backup.preferences.pref_pathBackupFolder
 import com.machiav3lli.backup.preferences.pref_prettyJson
 import com.machiav3lli.backup.preferences.pref_useYamlPreferences
 import com.machiav3lli.backup.preferences.pref_useYamlProperties
@@ -895,11 +896,13 @@ class OABX : Application() {
                     val storagePath = backupDirConfigured
                     if (storagePath.isEmpty()) {
                         Timber.e("backup storage location not configured")
+                        pref_pathBackupFolder.value = ""
                         throw StorageLocationNotConfiguredException()
                     }
                     val storageDir = StorageFile.fromUri(storagePath)
                     if (!storageDir.exists()) { //TODO hg42 for now only existing directories allowed
                         Timber.e("backup storage location not accessible: $storagePath")
+                        pref_pathBackupFolder.value = ""
                         throw FileUtils.BackupLocationInAccessibleException("Cannot access the root location '$storagePath'")
                     }
                     Timber.e("backup storage location found at ${storageDir.path}")
